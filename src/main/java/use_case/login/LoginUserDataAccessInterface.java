0e3c0c246900f54dd9a98c851df445dc0cbd5b13
Muhtasim Khan <muhtasim.khan@mail.utoronto.ca>
2024-10-19 09:35:59 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set the name for the current user.
+     * @param name the name of the user
+     */
+    void setCurrentUser(String name);
+
+    /**
+     * Returns the current user.
+     * @return the current user
+     */
+    String getCurrentUser();
 }
